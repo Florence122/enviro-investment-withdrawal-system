6db536385b7a3abd098edfc2f8d29651044b9915
Florence Chauke <florence@rad-tech.co.za>
2026-08-24 11:07:13 +0200

    Set up Spring Boot backend with H2 config

---
+++ backend/src/test/java/com/enviro/assessment/junior/florence/EnviroJuniorDeveloperAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.florence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroJuniorDeveloperAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
